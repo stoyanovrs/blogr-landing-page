b3198cb4038a2ae2015f3b0c9eb2d20be282b5ce
Rumen Stoyanov <stoyanovrs@gmail.com>
2022-12-13 19:28:57 +0200

    Desktop ready

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".main-nav");
+
+function handleClick() {
+  toggle.classList.toggle("open");
+  navigation.classList.toggle("show");
+}
+
+toggle.addEventListener("click", handleClick);
+
+let dropToggle = document.querySelectorAll(".drop-menu");
+let dropMenu = document.querySelector(".drop");
+
+function dropMenuFunction(e) {
+  e.target.classList.toggle("droped");
+}
+
+dropToggle.forEach((drop) => drop.addEventListener("click", dropMenuFunction));
